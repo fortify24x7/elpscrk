511257bf1c8eb5058144a23016742a345313e415
RussianOtter <elangs321@gmail.com>
2017-01-20 21:43:07 -0500

    RoBrute for iOS 10 Pythonista 3

---
+++ robrute.py
@@ -0,0 +1,187 @@
+import itertools, random, time, sys, console, os, sound
+from datetime import timedelta
+name = 'fsoceity'+'.data'
+yn = raw_input("Educated Brute Force Attack? y/n: ")
+time.sleep(0.5)
+dell = raw_input("Delete Last Log? y/n: ")
+if dell == "y":
+	try:
+		sound.play_effect("/System/Library/Audio/UISounds/payment_success.caf")
+		os.remove(name)
+	except:
+		pass
+time.sleep(1)
+if yn == "y":
+	console.set_color(0,1,1)
+	sys.stdout.write("elpscrk$: ")
+	console.set_color()
+	hints = raw_input("-psw.list-add ").split(";")
+	hint1 = hints[0]
+	hint2 = hints[1]
+	hint3 = hints[2]
+	hint4 = hints[3]
+	hint5 = hints[4]
+	if hint1 == "''":
+		hint1 = ''
+	if hint2 == "''":
+		hint2 = ''
+	if hint3 == "''":
+		hint3 = ''
+	if hint4 == "''":
+		hint4 = ''
+	if hint5 == "''":
+		hint5 = ''
+if yn != "y":
+	sys.exit()
+e = 2017 - 1980
+i = 0
+b = 0
+taken = 0
+console.set_color(0,1,1)
+sys.stdout.write("elpscrk$: ")
+console.set_color()
+a = (list(map("".join, itertools.permutations(raw_input("-Reverse ")))))
+c = len(a)
+lol = 1
+d = 1980
+start_time = time.time()
+for i in range(1, c + e):
+	combo = c + e
+	try:
+		sys.stdout.write("\r" + "Running Loop " + str(i + 1) + " of " + str(combo))
+		sys.stdout.flush()
+		time.sleep(0.02)
+		if yn == "y":
+			b = int(b + 1)
+			if b > c:
+				found = b
+				b = 0
+			a1 = hint1 + str(d)
+			a2 = hint2 + str(d)
+			a3 = hint3 + str(d)
+			a4 = hint4 + str(d)
+			a5 = str(d) + hint1
+			a6 = str(d) + hint2
+			a7 = str(d) + hint3
+			a8 = str(d) + hint4
+			a9 = hint1 + hint2 + str(d)
+			a10 = hint2 + hint1 + str(d)
+			a11 = hint3 + hint4 + str(d)
+			a12 = hint1 + str(d) + hint2
+			a13 = hint2 + str(d) + hint1
+			time.sleep(0.02)
+			b4 = a[b] + hint1 + str(d)
+			b5 = a[b] + hint2 + str(d)
+			b6 = a[b] + hint3 + str(d)
+			b7 = a[b] + hint4 + str(d)
+			b8 = a[b] + hint5 + str(d)
+			b9 = hint1 + hint5 + str(d)
+			b10 = hint2 + hint5 + str(d)
+			b11 = hint3 + hint5 + str(d)
+			b12 = hint4 + hint5 + str(d)
+			b13 = hint5 + hint1 + str(d)
+			b14 = hint5 + hint2 + str(d)
+			b15 = hint5 + hint3 + str(d)
+			b16 = hint5 + hint4 + std(d)
+			b17 = a[b] + str(d)
+			b19 = str(d)
+			b20 = "1a2b3c" + hint1 + str(d)
+			b21 = "1a2b3c" + hint2 + str(d)
+			b22 = "1a2b3c" + hint3 + str(d)
+			b23 = "1a2b3c" + hint4 + str(d)
+			taken = 0
+	except:
+		pass
+	if d == 2017:
+		d = 1980
+	d = d + 1
+	if lol == 1:
+		lol = 0
+		a19 = hint1 + hint5 + hint2 + hint5 + hint3 + hint5 + "321"
+		a20 = hint1 + "321"
+		a21 = hint2 + "321"
+		a22 = hint3 + "321"
+		a25 = hint4 + "321"
+		a26 = hint1 + "54321"
+		a27 = hint2 + "54321"
+		a28 = hint3 + "54321"
+		a29 = hint4 + "54321"
+		a14 = hint1 + a[b]
+		a15 = hint2 + a[b]
+		a16 = hint3 + a[b]
+		a17 = hint4 + a[b]
+		a18 = hint5 + a[b]
+		a30 = a[b] + hint1
+		b1 = a[b] + hint2
+		b2 = a[b] + hint3
+		b3 = a[b] + hint4
+		b18 = a[b]
+		c1 = hint1 + hint5 + hint2
+		c2 = hint1 + hint5 + hint3
+		c3 = hint1 + hint5 + hint4
+		c4 = hint2 + hint5 + hint3
+		c5 = hint3 + hint5 + hint2
+		c6 = hint1 + hint5
+		c7 = hint2 + hint5
+		c8 = hint3 + hint5
+		c9 = hint4 + hint5
+		c10 = hint4 + hint1			
+		c11 = hint5 + hint1
+		c12 = hint1
+		c13 = hint2
+		c14 = hint3		
+		c15 = hint4
+		c16 = "321" + hint1
+		c17 = "321" + hint2
+		c18 = "321" + hint3
+		c19 = "321" + hint4
+		c20 = "321" + hint5
+		c21 = "54321" + hint1
+		c22 = "54321" + hint2
+		c23 = "54321" + hint3
+		c24 = "54321" + hint4
+		c25 = "54321" + hint5
+		c26 = "123" + hint1
+		c27 = "123" + hint2	
+		c28 = "123" + hint3
+		c29 = "123" + hint4
+		c30 = "1a2b3c" + hint1
+		c31 = "1a2b3c" + hint2
+		c32 = "1a2b3c" + hint3
+		c33 = "1a2b3c" + hint4
+		d1 = a14 + str(d)
+		d2 = a15 + str(d)
+		d3 = a16 + str(d)
+		d4 = a17 + str(d)
+		d5 = a18 + str(d)
+		d6 = a19 + str(d)
+		d7 = a30 + str(d)
+		c34 = hint1 + str(random.randint(1,20))
+		c35 = hint2 + str(random.randint(1,20))
+		c36 = hint3 + str(random.randint(1,20))
+		c37 = hint4 + str(random.randint(1,20))
+		lol = 0
+	n = "\n"
+	print_list = a1 + n + a2 + n + a3 + n + a4 + n + a5 + n + a6 + n + a7 + n + a8 + n + a9 + n + a10 + n + a11 + n + a12 + n + a13 + n + b4 + n + b5 + n + b6 + n + b7 + n + b8 + n + b9 + n + b10 + n + b11 + n + b12 + n + b13 + n + b15
+	file = open(name,'a')
+	file.write(print_list)
+	time.sleep(0.001)
+if i == combo - 1:
+	n = "\n"
+	print_list2 = c1 + n + c2 + n + c3 + n + c4 + n + c5 + n + c6 + n + c7 + n + c8 + n + c9 + n + c10 + n + c11 + n + c12 + n + c13 + n + c14 + n + c15 + n + c16 + n + c17 + n + c18 + n + c19 + n + c20 + n + c21 + n + c22 + n + c23 + n + c24 + n + c25 + n + c26 + n + c27 + n + c28 + n + c29 + n + c30 + n + c31 + n + c32 + n + c33 + n + c34 + n + c35 + n + c36 + n + c37
+	debug_list = a19 + n + a20 + n + a21 + n + a22 + n + a25 + n + a26 + n + a27 + n + a28 + n + a29 + n + a14 + n + a15 + n + a16 + n + a17 + n + a18 + b18 + n + d1 + n + d2 + n + d3 + n + d4 + n + d5 + n + d6 + n + d7
+file.write(print_list2)
+file.write(debug_list)
+file.write("""\n----------------------------\n\n       LEAVE ME HERE!\n
+----------------------------\n
+""")
+file.close()
+elapsed_time = time.time() - start_time
+print "\n"
+time.sleep(0.5)
+times = str(timedelta(seconds=elapsed_time))
+console.set_color(0,1,1)
+sys.stdout.write("Time Elapsed: ")
+console.set_color()
+sys.stdout.write(str(times))
+print "\n"
