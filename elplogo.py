7d989baf782c532a729b2111ac0537e69688ea4b
RussianOtter <elangs321@gmail.com>
2017-01-24 18:02:27 -0500

    Create elplogo.py

---
+++ elplogo.py
@@ -0,0 +1,24 @@
+import console, time, sys
+console.set_color(0,1,1)
+time.sleep(2)
+print " " * 1 + "      _                     _    "
+time.sleep(0.1)
+print " " * 1 + "     | |                   | |   "
+time.sleep(0.1)
+print " " * 1 + "     | |                   | |   "
+time.sleep(0.1)
+print " " * 1 + "  ___| |____  ____ ___ _ __| | __"
+time.sleep(0.1)
+print " " * 1 + " / _ \ |  _ \/ __|/ __|  __| |/ /"
+time.sleep(0.1)
+print " " * 1 + "|  __/ | |_) \__ \ (__| |  |   < "
+time.sleep(0.1)
+print " " * 1 + " \___|_|  __/|___/\___|_|  |_|\_\ "
+time.sleep(0.1)
+print " " * 1 + "       | |                       "
+time.sleep(0.1)
+print " " * 1 + "       |_|                       "
+time.sleep(0.1)
+print " " * 1 + ""
+time.sleep(0.1)
+console.set_color()
